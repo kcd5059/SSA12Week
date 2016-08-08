a7cd175f1468f71d4a08e78fb67b390eb5787dc8
Kyle <kcd5059@gmail.com>
2016-08-08 09:08:02 -0400

    Fixes spacing and formatting for Knight classes

--- src/main/java/com/tiy/ssa/weekone/assignmentfour/Knight.java
+++ src/main/java/com/tiy/ssa/weekone/assignmentfour/Knight.java
@@ -1,52 +1,50 @@
 package com.tiy.ssa.weekone.assignmentfour;
+
 import java.util.ArrayList;
 
 public class Knight {
-	
-	final Location home;
-	Location current;
-	ArrayList<Location> positions = new ArrayList<Location>();
-	
 
-	public Knight(Location home) {
-		this.current = this.home = home;
-	}
-	
-	
-	public Location move(){
-		
-		if (isHome()) {
-			this.current = this.current.offset(1,  2);
-			positions.add(this.current);
-			return this.current;
-		} else {
-			return this.current;
-		}
-		
-	}
-	
-	public boolean isHome() {
-		return current.equals(home);
-	}
-	
-	public Location goHome() {
-		
-		this.current = this.current.offset(2, -1);
-		positions.add(this.current);
-		this.current = this.current.offset(-1, -2);
-		positions.add(this.current);
-		this.current = this.current.offset(-2, 1);
-		positions.add(this.current);
-		
-		return this.current;
-	}
-	
-	public Location getPosition() {
-		return this.current;
-	}
-	
-	
-	public Location getStart() {
-		return this.home;
-	}
+    final Location home;
+    Location current;
+    ArrayList<Location> positions = new ArrayList<Location>();
+
+    public Knight(Location home) {
+        this.current = this.home = home;
+    }
+
+    public Location move() {
+
+        if (isHome()) {
+            this.current = this.current.offset(1, 2);
+            positions.add(this.current);
+            return this.current;
+        } else {
+            return this.current;
+        }
+
+    }
+
+    public boolean isHome() {
+        return current.equals(home);
+    }
+
+    public Location goHome() {
+
+        this.current = this.current.offset(2, -1);
+        positions.add(this.current);
+        this.current = this.current.offset(-1, -2);
+        positions.add(this.current);
+        this.current = this.current.offset(-2, 1);
+        positions.add(this.current);
+
+        return this.current;
+    }
+
+    public Location getPosition() {
+        return this.current;
+    }
+
+    public Location getStart() {
+        return this.home;
+    }
 }
--- src/test/java/com/tiy/ssa/weekone/assignmentfour/KnightTests.java
+++ src/test/java/com/tiy/ssa/weekone/assignmentfour/KnightTests.java
@@ -5,53 +5,51 @@ import org.junit.Test;
 
 public class KnightTests {
 
-	@Test
-	public void locationTests() {
-		Knight k = new Knight(new Location(0, 0));
-		assertTrue(k.isHome());
-		Location after = k.move();
-		assertEquals(new Location(1, 2), after);
-		Location wentHome = k.goHome();
-		assertEquals(new Location(0, 0), wentHome);
-		assertTrue(uniquePathTest());
-		assertTrue(isLegal());
-	}
-	
-	
-	public boolean uniquePathTest() {
-		Knight k2 = new Knight(new Location(0, 0));
-		assertTrue(k2.isHome());
-		k2.move();
-		k2.goHome();
-		// Compare list of positions to ensure they are all unique
-		for (int i = 1; i < k2.positions.size() - 1; i++) {
-			for (int j = 1; j < i; j++) {
-				if (k2.positions.get(i).equals(k2.positions.get(j))) {
-					return false;
-				}
-			}
-		}
-		return true;
-	}
-	public boolean isLegal() {
-	    
+    @Test
+    public void locationTests() {
+        Knight k = new Knight(new Location(0, 0));
+        assertTrue(k.isHome());
+        Location after = k.move();
+        assertEquals(new Location(1, 2), after);
+        Location wentHome = k.goHome();
+        assertEquals(new Location(0, 0), wentHome);
+        assertTrue(uniquePathTest());
+        assertTrue(isLegal());
+    }
+
+    public boolean uniquePathTest() {
+        Knight k2 = new Knight(new Location(0, 0));
+        assertTrue(k2.isHome());
+        k2.move();
+        k2.goHome();
+        // Compare list of positions to ensure they are all unique
+        for (int i = 1; i < k2.positions.size() - 1; i++) {
+            for (int j = 1; j < i; j++) {
+                if (k2.positions.get(i).equals(k2.positions.get(j))) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    public boolean isLegal() {
+
         Knight k3 = new Knight(new Location(0, 0));
         assertTrue(k3.isHome());
         k3.move();
         k3.goHome();
-        
+
         for (int i = 0; i < k3.positions.size() - 1; i++) {
-                int differenceX = Math.abs(k3.positions.get(i+1).x) - Math.abs(k3.positions.get(i).x);
-                int differenceY = Math.abs(k3.positions.get(i+1).y) - Math.abs(k3.positions.get(i).y);
-                //System.out.println(differenceX + ", " + differenceY + ".... i = " + i);
-                if ( (differenceX > 2) || (differenceY > 2) ) {
-                    return false;
-                }
+            int differenceX = Math.abs(k3.positions.get(i + 1).x) - Math.abs(k3.positions.get(i).x);
+            int differenceY = Math.abs(k3.positions.get(i + 1).y) - Math.abs(k3.positions.get(i).y);
+            // System.out.println(differenceX + ", " + differenceY + ".... i = "
+            // + i);
+            if ((differenceX > 2) || (differenceY > 2)) {
+                return false;
+            }
         }
         return true;
     }
-	
-	
-	
 
 }
